aa7f09474e11e0b3c03c45c71f085559bca93ace
priyanshutiwari09 <priyanshuganeshtiwari@gmail.com>
2025-01-25 21:53:52 +0530

    Implement SocketIO

---
+++ backend/SocketIO/server.js
@@ -0,0 +1,35 @@
+const { Server } = require("socket.io");
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:4001",
+    methods: ["GET", "POST"]
+  }
+});
+
+const users = {};
+io.on("connection", (socket) => {
+  console.log("New Client Connected", socket.id);
+  const userId = socket.handshake.query.userId;
+  if (userId) {
+    users[userId] = socket.id;
+    console.log("LogUsers", users);
+  }
+
+  io.emit("getOnline", Object.keys(users));
+
+  socket.on("disconnect", () => {
+    console.log("Client Disconnected", socket.id);
+    delete users[userId];
+    io.emit("getOnline", Object.keys(users));
+  });
+});
+
+exports.app = app;
+exports.server = server;
+exports.io = io;
--- backend/index.js
+++ backend/index.js
@@ -3,32 +3,29 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter = require("./routes/userRoutes");
 const messageRouter = require("./routes/messageRoutes");
-const cors = require('cors');
-const cookieParser = require("cookie-parser")
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { app, server, io } = require("./SocketIO/server");
 
-
-
-const app = express();
+// const app = express();
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const URI = process.env.MONGODB_URI;
 
-app.use('/api/user', userRouter);
-app.use('/api/message', messageRouter);
-
-
+app.use("/api/user", userRouter);
+app.use("/api/message", messageRouter);
 
 try {
-    mongoose.connect(URI);
-    console.log("Database Connected...")
-  } catch (error) {
-    console.log(error);
+  mongoose.connect(URI);
+  console.log("Database Connected...");
+} catch (error) {
+  console.log(error);
 }
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
